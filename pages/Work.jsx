a9c92b9efa7411adb27f57c4da9cfb5b2d012ee4
shivani-520 <shivani_2002@icloud.com>
2026-06-29 20:50:24 +0100

    Cleanup

--- pages/Work.jsx
+++
@@ -1,8 +0,0 @@
-export default function Work() {
-  return (
-    <div style={{ color: "white", padding: "100px" }}>
-      <h1>Work Page</h1>
-      <p>This is your portfolio/work page.</p>
-    </div>
-  );
-}
